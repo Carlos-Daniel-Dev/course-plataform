888752090c11d2e244c4db25d3c665ef99ea2165
user <105173667+Carlos-Daniel-Dev@users.noreply.github.com>
2024-02-04 08:01:02 -0300

    removing comments from auth middleware

--- app/Http/Middleware/CheckAuthMiddleware.php
+++ app/Http/Middleware/CheckAuthMiddleware.php
@@ -10,11 +10,11 @@ class CheckAuthMiddleware
     public function handle($request, Closure $next)
     {
         if (Auth::check()) {
-            // O usuário está autenticado, permita o acesso à rota
+
             return $next($request);
+
         }
 
-        // O usuário não está autenticado, redirecione para a página de login
         return redirect('/login');
     }
 }
